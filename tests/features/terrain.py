3e1e7a745ef648e49e2d3c42d23b31f0a2577352
Rich Dammkoehler <rpd@noradltd.com>
2012-10-30 21:18:50 -0500

    Rich D: added more examples and some terrain

---
+++ tests/features/terrain.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from lettuce import *
+
+def isDebug():
+    rval = False
+    try:
+        if (os.environ['DEBUG']):
+            rval = True
+    except KeyError:
+        pass
+    return rval
+
+def report(message):
+    if (isDebug()):
+        print message
+
+@before.all
+def before_all():
+    report("About to do all")
+    
+@after.all
+def after_all(all):
+    report("Have done all %s" % (all))
+
+@before.each_feature
+def before_each_feature(feature):
+    report("About to do a feature %s" % (feature))
+
+@after.each_feature
+def after_each_feature(feature):
+    report("Have done a feature %s" % (feature))
+
+@before.each_scenario
+def before_each_scenario(scenario):
+    report("About to do a scenario %s" % (scenario))
+
+@after.each_scenario
+def after_each_scenario(scenario):
+    report("Have done a scenario %s" % (scenario))
+
+@before.each_step
+def before_each_step(step):
+    report("About to do a step %s\n" % (step))
+
+@after.each_step
+def after_each_step(step):
+    report("\nHave done a step %s" % (step))
